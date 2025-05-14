5c124586369cfe3a78a8918bbcc9b6d2a481e7d9
Tea Araki <teaaraki@hawaii.edu>
2025-05-14 02:50:56 +0000

    changes

--- index.js
+++ index.js
@@ -1,91 +1,59 @@
-//jshint esversion:6
+// jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
-// create a date object that requires the date.js file
 const date = require(__dirname + "/date.js");
 
 const app = express();
 
-// set an array for the default items in the list
+// Arrays
 let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
-// set an empty array for new work items
 let workItems = ["Show Up"];
-// create new array for fun items
 let funItems = ["Watch TV", "Read a Book"];
-// set an empty array for new weekend items
-let weekendItems = ["Relax", "Watch TV"];
+let finalExamItems = ["ICS 360", "MATH 241", "ENG 210", "HIST 151"]; // Add your UH courses here
 
-// set EJS as the viewing engine to display html
-app.set('view engine', 'ejs');
-
-// use body parser to parse html file
-app.use(bodyParser.urlencoded({extended: true}));
-
-// use Express to serve or display static files such as images, CSS, JS files etc.
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// default html file in web server
-app.get("/", function(req, res) {
-
-    //get the system date from the getDate function exported by the date.js file
-    let day = date.getDate();
-    
-    // use EJS render to display the day and the To Do List
-    res.render("list", {listTitle: day, newListItems: items});
-    
-});
-
-// display default to do list on the default root folder
-app.post("/", function(req, res) {
-    
-    // code allows items to be added to the regular list and work list
-    let item = req.body.newItem;
-    
-    // if route is /work, add to work list
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } 
-    
-    // if route is /fun, add to fun list
-    else if (req.body.list === "Fun") {
-        funItems.push(item);
-        res.redirect("/fun");
-    } 
-
-    // if route is /weekend, add to fun list
-    else if (req.body.list === "Weekend") {
-        weekendItems.push(item);
-        res.redirect("/weekend");
-    }
-
-    else {
-        items.push(item);
-        res.redirect("/");
-    }
-});
-
-// display work to do list on the localhost:3000/work route!
-app.get("/work", function(req, res){
-
+// Main List
+app.get("/", function (req, res) {
   let day = date.getDate();
-    res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
-// display fun to do list
-app.get("/fun", function(req, res){
-
-  let day = date.getDate();
-    res.render("list", {listTitle: "Fun To Do List", newListItems: funItems})
+// Handle Adding Items
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else if (req.body.list === "Fun") {
+    funItems.push(item);
+    res.redirect("/fun");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-// display weekend to do list
-app.get("/weekend", function(req, res){
-    res.render("list", {listTitle: "Weekend To Do List", newListItems: weekendItems})
+// Other Lists
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work To Do List", newListItems: workItems });
 });
 
-app.listen(3000, function() {
-console.log ("Server is running on port 3000")
+app.get("/fun", function (req, res) {
+  res.render("list", { listTitle: "Fun To Do List", newListItems: funItems });
+});
+
+// New Finals Route
+app.get("/finals", function (req, res) {
+  res.render("list", {
+    listTitle: "Final Exam To Do List",
+    newListItems: finalExamItems,
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
